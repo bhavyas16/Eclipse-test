55ca133062c6eec5e64117746bb4b504efca8920
chandrashekara <chandrashekara@MythriAssocates>
2019-04-10 15:34:22 +0530

    first commit

---
+++ test/src/test/firstu.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.*;
+
+public class firstu {
+	public static void main(String[] args) {
+		StringBuilder s1 = new StringBuilder("Java");
+		String s2 = "Love";
+		s1.append(s2);
+		s1.substring(4);
+		System.out.println(s1);
+		int foundAt = s1.indexOf(s2);
+		System.out.println(foundAt);
+		}
+}
